9460771167e9c7ef2fdff513bd862fa0f78ee13d
Jeremy Woertink <jeremywoertink@gmail.com>
2016-05-26 22:18:12 -0700

    adding a way to take a note and get a half step up

--- spec/notes_spec.cr
+++ spec/notes_spec.cr
@@ -5,5 +5,26 @@ describe Medley::Notes do
     it "returns an array of letters in C major scale order" do
       Medley::Notes.by_scale("Cmaj").should eq ["C", "D", "E", "F", "G", "A", "B", "C"]
     end
+
+    it "returns an array of letters in F major scale order" do
+      Medley::Notes.by_scale("Fmaj").should eq ["F", "G", "A", "Bb", "C", "D", "E", "F"]
+    end
+  end
+
+  describe ".halfstep_up" do
+    it "returns G# when given G" do
+      note = Medley::Notes.new("G")
+      note.halfstep_up.should eq "G#"
+    end
+
+    it "returns C when given B" do
+      note = Medley::Notes.new("B")
+      note.halfstep_up.should eq "C"
+    end
+
+    it "returns E when given D#" do
+      note = Medley::Notes.new("D#")
+      note.halfstep_up.should eq "E"
+    end
   end
 end
--- src/medley/notes.cr
+++ src/medley/notes.cr
@@ -1,9 +1,38 @@
 module Medley
   class Notes
     NOTE_NAMES = %w(A B C D E F G)
+    ALIASES = {"B#": "C", "Cb": "B", "E#": "F", "Fb": "E"}
 
+    # 1 1 .5 1 1 1 .5
     def self.by_scale(scale : String)
-      ["C", "D", "E", "F", "G", "A", "B", "C"]
+      scale.match(/(\w+)(maj|min)/)
+
+    end
+
+    def initialize(current_note : String)
+      @current_note = current_note
+    end
+
+    def natural?
+      NOTE_NAMES.includes?(@current_note)
+    end
+
+    def flat?
+      @current_note.includes?("b")
+    end
+
+    def sharp?
+      @current_note.includes?("#")
+    end
+
+    def halfstep_up
+      if natural?
+        note = "#{@current_note}#"
+        return ALIASES[note]? || note
+      elsif sharp?
+        idx = NOTE_NAMES.index(@current_note[0])
+        NOTE_NAMES[idx + 1]? || NOTE_NAMES[0]
+      end
     end
   end
 end
