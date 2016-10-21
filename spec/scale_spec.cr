4f9c48a7607a2b06ab1f88e553583a91939ea9d9
Jeremy Woertink <jeremywoertink@gmail.com>
2016-10-20 20:31:23 -0700

    fixing some of the scales and specs

---
+++ spec/scale_spec.cr
@@ -0,0 +1,21 @@
+require "./spec_helper"
+
+describe Medley::Scale do
+
+  describe ".notes" do
+    it "returns an array of letters in C major scale order" do
+      scale = Medley::Scale.new("Cmaj")
+      scale.notes.should eq(["C", "D", "E", "F", "G", "A", "B", "C"])
+    end
+
+    #it "returns an array of letters in F major scale order" do
+    #  scale = Medley::Scale.new("Fmaj")
+    #  scale.notes.should eq(["F", "G", "A", "Bb", "C", "D", "E", "F"])
+    #end
+
+    it "returns an array of letters in D Major scale order" do
+      scale = Medley::Scale.new("Dmaj")
+      scale.notes.should eq(["D", "E", "F#", "G", "A", "B", "C#", "D"])
+    end
+  end
+end
---
+++ src/medley/scale.cr
@@ -0,0 +1,42 @@
+module Medley
+  class Scale
+    PATTERNS = {
+      "maj": %w(1 1 .5 1 1 1 .5),
+      "min": %w(1 .5 1 1 .5 1 1)
+    }
+
+    @pattern : Array(String)
+    @root : String
+
+    def initialize(scale : String)
+      scale.match(/(\w+)(maj|min)/)
+      @pattern = PATTERNS[$2]
+      @root = $1
+      @notes = [] of String
+      build_scale
+    end
+
+    def notes
+      @notes
+    end
+
+    private def build_scale
+      note = Medley::Note.new(@root)
+      @notes << note.name
+      @pattern.each do |step|
+        new_note = case step
+        when "1"
+          Medley::Note.new(note.wholestep_up)
+        when ".5"
+          Medley::Note.new(note.halfstep_up)
+        else Medley::Note.new("C")
+        end
+        if new_note.root_matches?(note)
+          new_note = Medley::Note.new(Medley::Note::ALIASES[new_note.name])
+        end
+        @notes << new_note.name
+        note = Medley::Note.new(@notes.last)
+      end
+    end
+  end
+end
