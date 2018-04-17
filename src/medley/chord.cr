f0e663f7025c39dfce229ec717e7329e2207e48c
Russ Smith <russ@bashme.org>
2018-04-17 06:51:20 -0700

    A little refactoring.

--- src/medley/chord.cr
+++ src/medley/chord.cr
@@ -16,14 +16,10 @@ module Medley
     end
 
     private def build_chord(*pattern : Int32)
-      notes = @scale.notes
-      notes.pop
-      # TODO: Use a different method to loop back around
-      notes = notes + notes
-
-      root_index = notes.index(@root).as(Int32)
+      range = @scale.notes[0..-2] * 2
+      root_index = range.index(@root).as(Int32)
       pattern.each do |i|
-        @notes << notes[root_index + (i - 1)]
+        @notes << range[root_index + (i - 1)]
       end
     end
   end
